71cf65e4ccb8763f6faf34c28fb4ac5d9c957d83
sashank <sashank@srisai>
2015-09-25 02:01:53 -0500

    initial setup except server

--- myvenues/myvenues-webservices/src/main/java/com/ourits/myvenues/router/MyvenuesMaintenanceRouter.java
+++ myvenues/myvenues-webservices/src/main/java/com/ourits/myvenues/router/MyvenuesMaintenanceRouter.java
@@ -1,5 +1,22 @@
 package com.ourits.myvenues.router;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.ourit.myvenues.schema.helpers.TransactionStatusSchema;
+
+@Path("/maintenanceRouter")
 public class MyvenuesMaintenanceRouter {
 
+	@POST
+	@Path("/persistCustomerDetails")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_XML })
+	public TransactionStatusSchema persistCustomerDetails() {
+		return null;
+
+	}
 }
--- myvenues/myvenues-webservices/src/main/java/com/ourits/myvenues/router/MyvenuesRetrieveRouter.java
+++ myvenues/myvenues-webservices/src/main/java/com/ourits/myvenues/router/MyvenuesRetrieveRouter.java
@@ -1,5 +1,28 @@
 package com.ourits.myvenues.router;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.ourits.myvenues.business.service.CustomerRetrievalBusinessService;
+
+@Path("/retrievalRouter")
+@Stateless
 public class MyvenuesRetrieveRouter {
 
+	@EJB
+	private CustomerRetrievalBusinessService customerRetrievalBusinessService;
+
+	@POST
+	@Path("/retrieveCustomerDetails")
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML, MediaType.TEXT_XML })
+	public void retrieveCustomerDetails() {
+
+		customerRetrievalBusinessService.processCustomerDetailsRetrieval();
+	}
 }
